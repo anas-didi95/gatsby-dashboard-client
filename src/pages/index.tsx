e5f16587483763356df264bbd13b6df0e806ba76
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-14 19:06:17 +0800

    Configure and add bulma in project

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const IndexPage: React.FC<{}> = () => (
+  <section className="hero is-primary">
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">Primary title</h1>
+        <h2 className="subtitle">Primary subtitle</h2>
+      </div>
+    </div>
+  </section>
+)
+
+export default IndexPage
