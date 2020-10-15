633acafc58a078fc424ec5ca485b460251936a64
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-15 20:12:10 +0800

    Register form

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -4,15 +4,25 @@ interface IFormInput {
   label: string
   type: "text" | "password"
   error: string | undefined
+  register?: any
+  name: string
 }
 
-const FormInput: React.FC<IFormInput> = ({ label, type, error }) => (
+const FormInput: React.FC<IFormInput> = ({
+  label,
+  type,
+  error,
+  register,
+  name,
+}) => (
   <div className="field">
     <label className="label">{label}</label>
     <div className="control">
       <input
+        name={name}
         className={`input ${!!error ? "is-danger" : "is-link"}`}
         type={type}
+        ref={register}
       />
     </div>
     {!!error && <p className="help is-danger">{error}</p>}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useForm } from "react-hook-form"
 import Box from "../components/Box"
 import Button from "../components/Button"
 import ButtonGroup from "../components/ButtonGroup"
@@ -16,24 +17,55 @@ const IndexPage: React.FC<{}> = () => (
   </AppLayout>
 )
 
-const LoginForm: React.FC<{}> = () => (
-  <section className="container">
-    <div className="columns">
-      <div className="column" />
-      <article className="column is-6">
-        <Box>
-          <Form title="Login Form">
-            <FormInput label="Username" type="text" error="" />
-            <FormInput label="Password" type="password" error="" />
-            <ButtonGroup align="right">
-              <Button type="submit" color="primary" label="Login" />
-            </ButtonGroup>
-          </Form>
-        </Box>
-      </article>
-      <div className="column" />
-    </div>
-  </section>
-)
+const LoginForm: React.FC<{}> = () => {
+  type Form = {
+    username: string
+    password: string
+  }
+  const { register, handleSubmit, errors } = useForm<Form>()
+
+  const handler = {
+    onSubmit: (data: Form) => {
+      console.log("data:", data)
+    },
+  }
+
+  return (
+    <section className="container">
+      <div className="columns">
+        <div className="column" />
+        <article className="column is-6">
+          <Box>
+            <Form title="Login Form" onSubmit={handleSubmit(handler.onSubmit)}>
+              <FormInput
+                name="username"
+                label="Username"
+                type="text"
+                error={errors.username?.message}
+                register={register({ required: "Username is mandatory!" })}
+              />
+              <FormInput
+                name="password"
+                label="Password"
+                type="password"
+                error={errors.password?.message}
+                register={register({ required: "Password is mandatory!" })}
+              />
+              <ButtonGroup align="right">
+                <Button
+                  type="submit"
+                  color="primary"
+                  label="Login"
+                  onClick={handleSubmit(handler.onSubmit)}
+                />
+              </ButtonGroup>
+            </Form>
+          </Box>
+        </article>
+        <div className="column" />
+      </div>
+    </section>
+  )
+}
 
 export default IndexPage
