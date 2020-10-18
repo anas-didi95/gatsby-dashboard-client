2cf9d89e3e2c8f1b3af9541ade9ec63219a84307
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-18 20:04:19 +0800

    Test build and serve

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface IButton {
   isInverted?: boolean
   isOutlined?: boolean
   onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
-  isLoading: boolean
+  isLoading?: boolean
 }
 
 const Button: React.FC<IButton> = ({
@@ -19,16 +19,15 @@ const Button: React.FC<IButton> = ({
   onClick,
   isLoading,
 }) => (
-  <button
-    onClick={onClick}
-    type={type}
-    className={`button ${!!color ? "is-" + color : ""} ${
-      !!isInverted ? "is-inverted" : ""
-    } ${!!isOutlined ? "is-outlined" : ""} ${isLoading ? "is-loading" : ""}`}
-    disabled={isLoading}
-  >
-    {label}
-  </button>
-)
+    <button
+      onClick={onClick}
+      type={type}
+      className={`button ${!!color ? "is-" + color : ""} ${!!isInverted ? "is-inverted" : ""
+        } ${!!isOutlined ? "is-outlined" : ""} ${isLoading ? "is-loading" : ""}`}
+      disabled={isLoading}
+    >
+      {label}
+    </button>
+  )
 
 export default Button
