fa3684a3bc38e72c45f1f17c58cd86a8366390c3
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-19 20:54:26 +0800

    Add Status page

---
+++ src/pages/dashboard/status/index.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react"
+import Box from "../../../components/Box"
+import FormInput from "../../../components/FormInput"
+import AppLayout from "../../../layouts/AppLayout"
+
+const StatusPage: React.FC<{ location: any }> = ({ location }) => (
+  <AppLayout title="Status" location={location} needAuth={true}>
+    <section className="section">
+      <article className="container">
+        <div className="columns">
+          <div className="column" />
+          <div className="column is-10">
+            <nav className="breadcrumb is-medium" aria-label="breadcrumbs">
+              <ul>
+                <li>
+                  <a href="#" className="has-text-black">
+                    Dashboard
+                  </a>
+                </li>
+                <li className="is-active">
+                  <a href="#" className="has-text-black has-text-weight-bold">
+                    Status
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <br />
+            <div className="columns is-multiline">
+              <div className="column is-6">
+                <StatusPanel />
+              </div>
+              <div className="column is-6">
+                <StatusPanel />
+              </div>
+              <div className="column is-6">
+                <StatusPanel />
+              </div>
+            </div>
+          </div>
+          <div className="column" />
+        </div>
+      </article>
+    </section>
+  </AppLayout>
+)
+
+const StatusPanel: React.FC<{}> = () => {
+  const [security, setSecurity] = useState<string>("")
+  const [bot, setBot] = useState<string>("")
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response = await fetch(`${process.env.GATSBY_API_SECURITY}/ping`)
+        const responseBody = await response.json()
+        console.log(responseBody)
+        setSecurity(responseBody.outcome)
+      } catch (e) {
+        console.log(e)
+      }
+    })()
+  }, [])
+
+  return (
+    <div className="panel is-link">
+      <p className="panel-heading">Security</p>
+      <Box>
+        <div className="columns">
+          <div className="column is-8">
+            <div className="field">
+              <label className="label">URL</label>
+              <div className="control">
+                <p>{process.env.GATSBY_API_SECURITY}</p>
+              </div>
+            </div>
+          </div>
+          <div className="column is-4">
+            <div className="field">
+              <label className="label">Status</label>
+              <div className="control">
+                <p>
+                  {security === "UP" ? (
+                    <span className="tag is-success is-rounded has-text-weight-semibold">
+                      Online
+                    </span>
+                  ) : security === "DOWN" ? (
+                    <span className="tag is-danger is-rounded has-text-weight-semibold">
+                      Offline
+                    </span>
+                  ) : (
+                    <span className="tag is-warning is-rounded has-text-weight-semibold">
+                      Checking
+                    </span>
+                  )}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Box>
+    </div>
+  )
+}
+
+export default StatusPage
