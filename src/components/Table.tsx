0ef88723e94b030f093fcf22fef4123320f642a8
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-21 21:01:48 +0800

    Add query to get user list

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,30 +6,28 @@ interface ITable {
   children?: ReactNode
 }
 const Table: React.FC<ITable> = ({ headers, widths, children }) => (
-  <div className="table-container">
-    <table className="table is-striped is-bordered is-fullwidth is-hoverable">
-      <thead>
+  <table className="table is-striped is-bordered is-fullwidth is-hoverable">
+    <thead>
+      <tr>
+        {headers.map((header, i) => (
+          <th key={header} style={{ width: `${widths[i]}%` }}>
+            {header}
+          </th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {!!children ? (
+        children
+      ) : (
         <tr>
-          {headers.map((header, i) => (
-            <th key={header} style={{ width: `${widths[i]}%` }}>
-              {header}
-            </th>
-          ))}
+          <td className="has-text-centered" colSpan={headers.length}>
+            No record found
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        {!!children ? (
-          children
-        ) : (
-          <tr>
-            <td className="has-text-centered" colSpan={headers.length}>
-              No record found
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
-  </div>
+      )}
+    </tbody>
+  </table>
 )
 
 export default Table
--- src/pages/dashboard/security/user/index.tsx
+++ src/pages/dashboard/security/user/index.tsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 import Breadcrumb from "../../../../components/Breadcrumb"
 import Panel from "../../../../components/Panel"
 import Table from "../../../../components/Table"
 import AppLayout from "../../../../layouts/AppLayout"
+import AuthContext from "../../../../utils/contexts/AuthContext"
 
 const SecurityUserListPage: React.FC<{ location: any }> = ({ location }) => (
   <AppLayout title="Security - User" location={location} needAuth={true}>
@@ -22,13 +23,60 @@ const SecurityUserListPage: React.FC<{ location: any }> = ({ location }) => (
   </AppLayout>
 )
 
-const UserListTable: React.FC<{}> = () => (
-  <Panel title="User Listing" color="is-link">
-    <Table
-      headers={["No", "Username", "Full Name"]}
-      widths={[10, 40, 50]}
-    ></Table>
-  </Panel>
-)
+const UserListTable: React.FC<{}> = () => {
+  type TUser = {
+    id: string
+    username: string
+    fullName: string
+    version: number
+  }
+  const [userList, setUserList] = useState<TUser[]>([])
+  const authContext = useContext(AuthContext)
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch(
+        `${process.env.GATSBY_API_SECURITY}/graphql`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authContext.getAccessToken()}`,
+          },
+          body: JSON.stringify({
+            query: `
+            query {
+              getUserList {
+                id
+                username
+                fullName
+                version
+              }
+            }
+          `,
+            variables: null,
+          }),
+        }
+      )
+      const responseBody = await response.json()
+      setUserList(responseBody.data.getUserList)
+    })()
+  }, [])
+
+  return (
+    <Panel title="User Listing" color="is-link">
+      <Table headers={["No", "Username", "Full Name"]} widths={[10, 40, 50]}>
+        {userList.map((user, i) => (
+          <tr key={`${user.id}`}>
+            <td>{i + 1}</td>
+            <td>{user.username}</td>
+            <td>{user.fullName}</td>
+          </tr>
+        ))}
+      </Table>
+    </Panel>
+  )
+}
 
 export default SecurityUserListPage
