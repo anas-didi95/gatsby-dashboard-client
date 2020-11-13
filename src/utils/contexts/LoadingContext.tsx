dfb84d166b4459b3016da2447bb3ecb5c40faf8e
Anas Juwaidi <anas.didi95@gmail.com>
2020-11-13 20:47:47 +0800

    Add Loading context

---
+++ src/utils/contexts/LoadingContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, ReactNode, useState } from "react"
+
+interface ILoadingContext {
+  isLoading: () => boolean
+  onLoading: () => void
+  offLoading: () => void
+}
+const LoadingContext = createContext<ILoadingContext>({
+  isLoading: () => false,
+  onLoading: () => { },
+  offLoading: () => { }
+})
+
+const LoadingProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  type TData = { isLoading: boolean }
+  const [data, setData] = useState<TData>({ isLoading: false })
+
+  const isLoading = () => data.isLoading
+  const onLoading = () => setData(prev => ({ ...prev, isLoading: true }))
+  const offLoading = () => setData(prev => ({ ...prev, isLoading: false }))
+
+  return <LoadingContext.Provider value={{ isLoading, onLoading, offLoading }}>{children}</LoadingContext.Provider>
+}
+
+export default LoadingContext
+export { LoadingProvider }
